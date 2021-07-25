4f3eb5cc2e5915b931a6de6bc941728aea6b751b
TAYTS <taytzushieh@gmail.com>
2021-07-25 22:33:39 +0800

    Add mock data for the workshops section

---
+++ src/sections/workshops/workshopData.ts
@@ -0,0 +1,38 @@
+export const workshopData = [
+  {
+    id: 1,
+    title: 'Repurposing Scrap Materials for Prototype Design',
+    description:
+      'Repurposing Scrap Materials for Prototype DesignIn this workshop, you will learn how to upcycle scrap material into something usable for your prototype. Participants will be guided on how to operate the fabrication tools in the fabrication lab.',
+    info: '1.5hours, Conducted by Makerspace',
+    videoURL: 'https://www.youtube.com/embed/TcOmMyEjLEc',
+    thumbnailURL: './bridge.jpg',
+  },
+  {
+    id: 2,
+    title: 'ESO32 Web Server & BLE',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor at risus viverra adipiscing. Lacus laoreet non curabitur gravida arcu.',
+    info: '1.5hours, Conducted by Makerspace',
+    videoURL: 'https://www.youtube.com/embed/O8174TdqENc',
+    thumbnailURL: './bridge.jpg',
+  },
+  {
+    id: 3,
+    title: 'Fusion 360 Beginner',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor at risus viverra adipiscing. Lacus laoreet non curabitur gravida arcu.',
+    info: '1.5hours, Conducted by Makerspace',
+    videoURL: 'https://www.youtube.com/embed/By7rLend4W0',
+    thumbnailURL: './bridge.jpg',
+  },
+  {
+    id: 4,
+    title: 'Utilising Alternate Energy Sources in Prototyping',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor at risus viverra adipiscing. Lacus laoreet non curabitur gravida arcu.',
+    info: '1.5hours, Conducted by Makerspace',
+    videoURL: 'https://www.youtube.com/embed/TcOmMyEjLEc',
+    thumbnailURL: './bridge.jpg',
+  },
+];
