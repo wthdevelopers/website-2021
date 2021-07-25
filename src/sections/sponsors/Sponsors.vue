9e7466afa9f1308c6b62917d5b7384fd9962ee0a
James R T <jamestiotio@gmail.com>
2021-07-25 17:48:29 +0800

    Add 3DC logo

--- src/sections/sponsors/Sponsors.vue
+++ src/sections/sponsors/Sponsors.vue
@@ -147,6 +147,11 @@ export default {
               name: "SUTD Productions",
               photoNormal: "SUTD-Productions-Logo",
               photoWhite: "SUTD-Productions-Logo-white"
+            },
+            {
+              name: "3DC",
+              photoNormal: "3dc",
+              photoWhite: "3dc-white"
             }
           ]
         }
