86ebf46f238335611a2a551d138c31b5473e30bf
James R T <jamestiotio@gmail.com>
2021-08-12 22:11:56 +0800

    Add sticky navbar

---
+++ src/sections/hero/NavbarBlock.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar-block-bg"></div>
+</template>
+
+<style scoped>
+.navbar-block-bg {
+  min-height: 12.5%;
+  width: 100%;
+  display: inline-block;
+  z-index: 2;
+}
+</style>
