56c15c5e6395c10ace908c73cd10aa892009ee6d
TAYTS <taytzushieh@gmail.com>
2021-08-20 23:28:02 +0800

    Update registration note

--- src/sections/hero/HeroContent.vue
+++ src/sections/hero/HeroContent.vue
@@ -23,7 +23,7 @@
           </button>
         </template>
         <template v-slot:content>
-          <Para className="tooltip__text">*All participants need to submit a registration form</Para>
+          <Para className="tooltip__text">Note that all participants would need to submit a registration form.</Para>
         </template>
       </Tooltip>
     </div>
