4a5f2664c19bef512f4dc32f34235c45e6661e11
James R T <jamestiotio@gmail.com>
2021-09-16 19:36:49 +0800

    Add embedded workshop video links

--- src/sections/workshops/workshopData.ts
+++ src/sections/workshops/workshopData.ts
@@ -5,7 +5,7 @@ export const workshopData = [
     description:
       "Want to prototype and 3D-print but don't know where to start? This workshop will cover all the basics of CAD-ing using Fusion 360 as well as preparing your models to be 3D-printed by CURA software.",
     info: 'Conducted by Mech D',
-    videoURL: '',
+    videoURL: 'https://www.youtube.com/embed/_yrS5vuvsoo',
     thumbnailURL: './mechd.png',
   },
   {
@@ -23,7 +23,7 @@ export const workshopData = [
     description:
       'React is one of the most popular front-end frameworks available. This workshop will give you a brief refresher on JavaScript, HTML, and CSS followed by an introduction to how React works and a demo. This will equip you with the basic understanding needed to start with your own React application.',
     info: 'Conducted by 3DC',
-    videoURL: '',
+    videoURL: 'https://www.youtube.com/embed/5IfpNnFWdbw',
     thumbnailURL: './react.jpg',
   },
   {
@@ -41,7 +41,7 @@ export const workshopData = [
     description:
       'FastAPI allows you to build a Python backend server super fast - complete with type checking, hot reload and also automatically generated Swagger documentation to quickly test your APIs. Learn how to hook one up with Firebase to spin up a backend in no time!',
     info: 'Conducted by 3DC',
-    videoURL: '',
+    videoURL: 'https://www.youtube.com/embed/SD_Be1m_EXE',
     thumbnailURL: './python.jpg',
   },
   {
@@ -49,7 +49,7 @@ export const workshopData = [
     title: 'Alternative Energy',
     description: 'Introduce alternative energy (photovoltaic) and design concepts to participants.',
     info: 'Conducted by Energy Club',
-    videoURL: '',
+    videoURL: 'https://www.youtube.com/embed/wEsBvtBHi10',
     thumbnailURL: './energyclub.jpg',
   },
 ];
